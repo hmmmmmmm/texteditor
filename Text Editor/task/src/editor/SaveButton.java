0bb4575e2e6c6d6d47d3d08cbafc84813d848d8c
Eilidh Martin <me@eilidh.online>
2020-07-10 15:33:21 +0100

    Redoing save/load as simpler actions rather than subclassing SaveButton and LoadButton

--- Text Editor/task/src/editor/SaveButton.java
+++ Text Editor/task/src/editor/SaveButton.java
@@ -1,39 +1,39 @@
-package editor;
-
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.*;
-
-public class SaveButton extends JButton implements ActionListener {
-    private final JTextField filenameField;
-    private final JTextArea textArea;
-
-    SaveButton(final JTextField textField, final JTextArea textArea){
-        super();
-        this.filenameField = textField;
-        this.textArea = textArea;
-
-        this.addActionListener(this);
-    }
-
-    public void actionPerformed(ActionEvent save){
-        File file = new File(filenameField.getText());
-        BufferedWriter fout = null;
-        try {
-            fout = new BufferedWriter(new FileWriter(file));
-
-            textArea.write(fout);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (fout != null) {
-                    fout.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
+//package editor;
+//
+//import javax.swing.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.io.*;
+//
+//public class SaveButton extends JButton implements ActionListener {
+//    private final JTextField filenameField;
+//    private final JTextArea textArea;
+//
+//    SaveButton(final JTextField textField, final JTextArea textArea){
+//        super();
+//        this.filenameField = textField;
+//        this.textArea = textArea;
+//
+//        this.addActionListener(this);
+//    }
+//
+//    public void actionPerformed(ActionEvent save){
+//        File file = new File(filenameField.getText());
+//        BufferedWriter fout = null;
+//        try {
+//            fout = new BufferedWriter(new FileWriter(file));
+//
+//            textArea.write(fout);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (fout != null) {
+//                    fout.close();
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
